f771557e92af38b46d63cf2ec36f5d7c18f0faec
hippolytej <hippolyte.jacomet@gmail.com>
2017-02-10 00:31:21 +0100

    Et voila

--- project_code/hippo.R
+++ project_code/hippo.R
@@ -25,32 +25,60 @@ data1 = data1temp[order(data1temp$Year),]
 data1$Total_amount = as.numeric(data1$Total_amount)
 
 # Plot the results
-gr = ggplot(data = data1, aes(x = Year, y = Total_amount, fill = ActType)) +
+gr1 = ggplot(data = data1, aes(x = Year, y = Total_amount, fill = ActType)) +
     geom_bar(stat = "identity", alpha = 0.7, position="dodge")
-print(gr)
+print(gr1)
 
 
-# 2.    Amount repartitions ---------------------------
+# 2.    Amount repartitions ----------------------------------------------------------------------
 
-query3 ='SELECT ActType, Amount
+query2 ='SELECT ActType, Amount
          FROM charity.acts'
 
-data3 = sqlQuery(db,query3)
-head(data3)
+data2 = sqlQuery(db,query2)
+head(data2)
 
-PAs = as.data.frame(cut(data3$Amount[data3$ActType=='PA'],breaks = seq(0, 100, by = 5)))
+PAs = as.data.frame(cut(data2$Amount[data2$ActType=='PA'],breaks = seq(0, 100, by = 5)))
 colnames(PAs)=c('Amounts')
 PAs['ActType'] = 'PA'
-DOs = as.data.frame(cut(data3$Amount[data3$ActType=='DO'], breaks = seq(0, 100, by = 5)))
+DOs = as.data.frame(cut(data2$Amount[data2$ActType=='DO'], breaks = seq(0, 100, by = 5)))
 colnames(DOs)=c('Amounts')
 DOs['ActType'] = 'DO'
-data3= rbind(PAs, DOs)
+data2= rbind(PAs, DOs)
 
 # Minor correction for aestethical purposes
-data3 = rbind(data3, c("(70,75]", "PA"))
-data3 = rbind(data3, c("(80,85]", "PA"))
+data2 = rbind(data2, c("(70,75]", "PA"))
+data2 = rbind(data2, c("(80,85]", "PA"))
 
 # Plot the results
-gr = ggplot(data= data3, aes(x=Amounts, fill = ActType))+
+gr2 = ggplot(data= data2, aes(x=Amounts, fill = ActType))+
     geom_histogram(alpha=.7, stat="count", position="dodge")
-print(gr)
+print(gr2)
+
+# 2.    Pareto Rule ------------------------------------------------------------------------------
+
+query3 ='SELECT ContactId as Donators, SUM(Amount) as Sum_of_donations
+         FROM charity.acts
+         WHERE ActType LIKE "DO"
+         GROUP BY 1
+         ORDER BY 2 DESC'
+
+data3 = sqlQuery(db,query3)
+dim(data3)
+
+data3$Donators = seq(0,1, length.out = nrow(data3))
+data3$Sum_of_donations = as.numeric(data3$Sum_of_donations/data3$Sum_of_donations[1])
+head(data3)
+
+
+# Plot the results
+gr3 = ggplot(data = data3, aes(x = Donators, y = Sum_of_donations, colour = "Sum of donations")) +
+    geom_line(alpha = 0.7, size = 1) +
+    geom_line(data = data3,alpha = 0.7, size = 1, aes(colour = "Cumulative sum", x= Donators, y=cumsum(Sum_of_donations)/sum(data3$Sum_of_donations)))+ 
+    geom_vline(xintercept = .2, colour="#0e492d", linetype = "longdash") +
+    geom_hline(yintercept = .8, colour="#0e492d", linetype = "longdash") +
+    scale_colour_discrete("")
+    
+
+print(gr3)
+
