587fa5b4747cd44b20d24a91d1d45fe6711e0389
Xiang Binxiang <binxiang.xiang@student.ecp.fr>
2017-02-10 00:25:15 +0100

    add plots by day and map with all data

--- project_code/binxiang.R
+++ project_code/binxiang.R
@@ -10,11 +10,14 @@ db = odbcConnect("MySQL", uid="root") # change if needed with name of your ODBC 
 
 #-    How much money is collected every year?
 
+
 query1 ="SELECT YEAR(ActDate) as Year, SUM(Amount) as Total_amount
           FROM charity.acts
           GROUP BY Year"
 
 data1 = sqlQuery(db,query1)
+
+
 #-    How many donations 
 
 #-     Evolution of average amount per donation for DO and PA through the different years 
@@ -31,6 +34,8 @@ print(plot2)
 
 
 
+
+
 #-    Evolution of total number of donations for DO and PA through the different years
 # we will first estimate the number of donations (DO and PA) for December 2013 by taking
 # the values of the previous year, there are of course more complex methods
@@ -41,6 +46,9 @@ query3 = "SELECT * FROM (
           GROUP BY Year, Month,ActType) A
           WHERE A.Year = 2012 AND A.Month = 12"
 data3 = sqlQuery(db,query3)
+
+
+
 
 # Now we query the data grouping by year and ActType
 query4 = "
@@ -61,7 +69,10 @@ plot4 = ggplot(data = data4, aes(x=Year, y=Nb,col=ActType)) + geom_point() + geo
   xlim(c(2002,2013)) + ylim(c(0,max(data4$Nb)+10))
 print(plot4)
 
-#- Distribution of average amount of donations for each month over the different years
+
+
+
+#- average amount of donations for each month over the different years
 
 query5 = "
           SELECT MONTH(ActDate) as Month, ActType, AVG(Amount) as Avg_amount
@@ -78,7 +89,10 @@ plot5 = ggplot(data = data5,aes(x=Month,y=Avg_amount,color=ActType)) + geom_poin
 print(plot5)
 
 
-#- Distribution of average number of donations for each month
+
+
+
+#- average number of donations for each month
 
 query6 = "SELECT A.Month, A.ActType, AVG(A.Nb) as Avg_Nb
           FROM
@@ -92,165 +106,41 @@ plot6 = ggplot(data = data6,aes(x=Month,y=Avg_Nb,color=ActType)) + geom_line() +
   xlim(c(0,13)) + ylim(c(0,max(data6$Avg_Nb)+5))
 
 print(plot6)
-# Lets separate the data in each year (2003-2013)
-acts2003 = acts[acts$ActDate >= as.Date("2003-01-01", "%Y-%m-%d") & acts$ActDate <= as.Date("2003-12-31", "%Y-%m-%d"), ]
-acts2004 = acts[acts$ActDate >= as.Date("2004-01-01", "%Y-%m-%d") & acts$ActDate <= as.Date("2004-12-31", "%Y-%m-%d"), ]
-acts2005 = acts[acts$ActDate >= as.Date("2005-01-01", "%Y-%m-%d") & acts$ActDate <= as.Date("2005-12-31", "%Y-%m-%d"), ]
-acts2006 = acts[acts$ActDate >= as.Date("2006-01-01", "%Y-%m-%d") & acts$ActDate <= as.Date("2006-12-31", "%Y-%m-%d"), ]
-acts2007 = acts[acts$ActDate >= as.Date("2007-01-01", "%Y-%m-%d") & acts$ActDate <= as.Date("2007-12-31", "%Y-%m-%d"), ]
-acts2008 = acts[acts$ActDate >= as.Date("2008-01-01", "%Y-%m-%d") & acts$ActDate <= as.Date("2008-12-31", "%Y-%m-%d"), ]
-acts2009 = acts[acts$ActDate >= as.Date("2009-01-01", "%Y-%m-%d") & acts$ActDate <= as.Date("2009-12-31", "%Y-%m-%d"), ]
-acts2010 = acts[acts$ActDate >= as.Date("2010-01-01", "%Y-%m-%d") & acts$ActDate <= as.Date("2010-12-31", "%Y-%m-%d"), ]
-acts2011 = acts[acts$ActDate >= as.Date("2011-01-01", "%Y-%m-%d") & acts$ActDate <= as.Date("2011-12-31", "%Y-%m-%d"), ]
-acts2012 = acts[acts$ActDate >= as.Date("2012-01-01", "%Y-%m-%d") & acts$ActDate <= as.Date("2012-12-31", "%Y-%m-%d"), ]
-acts2013 = acts[acts$ActDate >= as.Date("2013-01-01", "%Y-%m-%d") & acts$ActDate <= as.Date("2013-12-31", "%Y-%m-%d"), ]
-
-amountperyear = c(sum(acts2003$Amount), sum(acts2004$Amount), sum(acts2005$Amount), sum(acts2006$Amount), sum(acts2007$Amount), sum(acts2008$Amount), sum(acts2009$Amount), sum(acts2010$Amount), sum(acts2011$Amount), sum(acts2012$Amount), sum(acts2013$Amount))
-
-# Plot the results
-barplot(amountperyear, main = "Amount collected per year", xlab = "Year", ylab = "Amount", type = "b", names.arg =seq(2003, 2013))
-
-# Total money collected
-sum(amountperyear)
-
-# Mean money collected per year (NOT taking into account the not-complete years)
-mean(amountperyear[2:10])
-
-#-    How much money is collected through classic donations (ActType=’DO’) and automatic deductions (ActType=’PA’)?
-
-# Seperating both donation types
-actsDO = acts[acts$ActType == "DO", ]
-actsPA = acts[acts$ActType == "PA", ]
-
-# Total money collected through classic donations (ActType=’DO’)
-sum(actsDO$Amount)
-
-# Total money collected through automatic deductions (ActType=’PA’)
-sum(actsPA$Amount)
-
-# Decomposition per year for classic donations (ActType=’DO’)
-acts2003_DO = acts2003[acts2003$ActType == "DO", ]
-acts2004_DO = acts2004[acts2004$ActType == "DO", ]
-acts2005_DO = acts2005[acts2005$ActType == "DO", ]
-acts2006_DO = acts2006[acts2006$ActType == "DO", ]
-acts2007_DO = acts2007[acts2007$ActType == "DO", ]
-acts2008_DO = acts2008[acts2008$ActType == "DO", ]
-acts2009_DO = acts2009[acts2009$ActType == "DO", ]
-acts2010_DO = acts2010[acts2010$ActType == "DO", ]
-acts2011_DO = acts2011[acts2011$ActType == "DO", ]
-acts2012_DO = acts2012[acts2012$ActType == "DO", ]
-acts2013_DO = acts2013[acts2013$ActType == "DO", ]
-
-amountperyear_DO = c(sum(acts2003_DO$Amount), sum(acts2004_DO$Amount), sum(acts2005_DO$Amount), sum(acts2006_DO$Amount), sum(acts2007_DO$Amount), sum(acts2008_DO$Amount), sum(acts2009_DO$Amount), sum(acts2010_DO$Amount), sum(acts2011_DO$Amount), sum(acts2012_DO$Amount), sum(acts2013_DO$Amount))
-
-# Plot the results
-barplot(amountperyear_DO, main = "Amount collected per year - DO", xlab = "Year", ylab = "Amount", type = "b", names.arg =seq(2003, 2013))
-
-# Decomposition per year for automatic deductions (ActType=’PA’)
-acts2003_PA = acts2003[acts2003$ActType == "PA", ]
-acts2004_PA = acts2004[acts2004$ActType == "PA", ]
-acts2005_PA = acts2005[acts2005$ActType == "PA", ]
-acts2006_PA = acts2006[acts2006$ActType == "PA", ]
-acts2007_PA = acts2007[acts2007$ActType == "PA", ]
-acts2008_PA = acts2008[acts2008$ActType == "PA", ]
-acts2009_PA = acts2009[acts2009$ActType == "PA", ]
-acts2010_PA = acts2010[acts2010$ActType == "PA", ]
-acts2011_PA = acts2011[acts2011$ActType == "PA", ]
-acts2012_PA = acts2012[acts2012$ActType == "PA", ]
-acts2013_PA = acts2013[acts2013$ActType == "PA", ]
-
-amountperyear_PA = c(sum(acts2003_PA$Amount), sum(acts2004_PA$Amount), sum(acts2005_PA$Amount), sum(acts2006_PA$Amount), sum(acts2007_PA$Amount), sum(acts2008_PA$Amount), sum(acts2009_PA$Amount), sum(acts2010_PA$Amount), sum(acts2011_PA$Amount), sum(acts2012_PA$Amount), sum(acts2013_PA$Amount))
-
-# Plot the results
-barplot(amountperyear_PA, main = "Amount collected per year - PA", xlab = "Year", ylab = "Amount", type = "b", names.arg = seq(2003, 2013))
-
-# Plot both together
-dat <- rbind(amountperyear_DO, amountperyear_PA)
-colnames(dat) = seq(2003, 2013)
-rownames(dat) = c("DO", "PA")
-barplot(as.matrix(dat), main = "Decomposed amount collected per year", xlab = "Year", ylab = "Amount ($)", col = c("blue","red"))
-legend("topleft", fill = c("red", "blue"), legend = c("PA", "DO"), bty = "n")
-
-#-    How many contacts are there in the database? How many donors?
-
-# Unique contact ids in the data base (normally equivalent to the number of contacts in the database)
-length(unique(contacts$ContactId)) 
-
-# Lets take a look at the contact ids that are doubled in the contact data base
-n_occur <- data.frame(table(contacts$ContactId))
-n_occur[n_occur$Freq > 1,]
-contacts[contacts$ContactId == 10650, ]
-contacts[contacts$ContactId == 98460, ]
-contacts[contacts$ContactId == 166900, ]
-contacts[contacts$ContactId == 188110, ]
-# Its okay, they are just double lines, so we have indeed 24689 unique contacts in the data base. 
-
-# Unique contact ids in the donor data base 
-length(unique(acts$ContactId)) 
-
-# Lets just make sure each line in the acts corresponds to a true donation
-acts[acts$Amount < 0,]
-acts[acts$Amount == 0,]
-# Good, so we indeed have 18945 unique donors in our data base. 
 
 
-#-    How many donors have been active each and every year? 
-# Change the list accordingly if we need to check less years
-donors_active_each_year = Reduce(intersect, list(acts2003$ContactId, acts2004$ContactId, acts2005$ContactId, acts2006$ContactId, acts2007$ContactId, acts2008$ContactId, acts2009$ContactId, acts2010$ContactId, acts2011$ContactId, acts2012$ContactId, acts2013$ContactId))
-length(donors_active_each_year)
 
-#-    Is the average donation amount increasing or decreasing? How much is it?
-average_donation = c(mean(acts2003$Amount), mean(acts2004$Amount), mean(acts2005$Amount), mean(acts2006$Amount), mean(acts2007$Amount), mean(acts2008$Amount), mean(acts2009$Amount), mean(acts2010$Amount), mean(acts2011$Amount), mean(acts2012$Amount), mean(acts2013$Amount))
-average_donation_DO = c(mean(acts2003_DO$Amount), mean(acts2004_DO$Amount), mean(acts2005_DO$Amount), mean(acts2006_DO$Amount), mean(acts2007_DO$Amount), mean(acts2008_DO$Amount), mean(acts2009_DO$Amount), mean(acts2010_DO$Amount), mean(acts2011_DO$Amount), mean(acts2012_DO$Amount), mean(acts2013_DO$Amount))
-average_donation_PA = c(mean(acts2003_PA$Amount), mean(acts2004_PA$Amount), mean(acts2005_PA$Amount), mean(acts2006_PA$Amount), mean(acts2007_PA$Amount), mean(acts2008_PA$Amount), mean(acts2009_PA$Amount), mean(acts2010_PA$Amount), mean(acts2011_PA$Amount), mean(acts2012_PA$Amount), mean(acts2013_PA$Amount))
 
-plot(average_donation_DO, type = 'l', xaxt = "n", col = "blue", ylim=c(0, 140), main = "Average donation amount", ylab = "Amount ($)", xlab = "Year")
-lines(average_donation_PA, col = "red")
-lines(average_donation, col = "black")
-axis(1, 1:11, 2003:2013)
-legend("topleft", col = c("black","red", "blue"), legend = c("Total", "PA", "DO"), bty = "n", lty=c(1,1,1))
 
-#-    How many “new” donors were acquired every year?
+#- average amount per donation for each day of the week 
 
-# First lets create an intermediate function
-add_zeros <- function(data){
-    new_data = rep(0,length(acts2012$ContactId + 1))
-    for (i in 1:length(acts2012$ContactId)){
-        if (i <= length(data)){
-            new_data[i] = data[i]
-        } else if (i > length(data)){
-            new_data[i] = 0
-        }
-    } 
-    return(new_data)
-}
+query7 = "
+          SELECT DAYOFWEEK(ActDate) as Day, ActType, AVG(Amount) as Avg_amount
+          FROM charity.acts 
+          GROUP BY Day,ActType"
+data7 = sqlQuery(db,query7)
 
-# And an intermediate database
-actsyears = cbind(add_zeros(acts2003$ContactId), add_zeros(acts2004$ContactId),
-                  add_zeros(acts2005$ContactId), add_zeros(acts2006$ContactId), 
-                  add_zeros(acts2007$ContactId), add_zeros(acts2008$ContactId), 
-                  add_zeros(acts2009$ContactId), add_zeros(acts2010$ContactId), 
-                  add_zeros(acts2011$ContactId), add_zeros(acts2012$ContactId), 
-                  add_zeros(acts2013$ContactId))
 
-# Now for the main function : 
-new_donors <- function(year){
-    index = year - 2002
-    test = actsyears[,1]
-    for (i in 1:(index-1)){
-        test = list(test, actsyears[,i])
-    }
-    donors = setdiff(actsyears[,index], Reduce(intersect, test)) # setdiff(x, y) returns the elements of x not present in y
-    return(unique(donors))
-}
-new_donors(2006) # Returns the all new donors adquired in year 2006
+plot7 = ggplot(data = data7,aes(x=Day,y=Avg_amount,color=ActType)) + geom_line() + geom_point()+
+  labs(title = 'Average amount per donation by day of week',y = 'Average amount per donation') +
+  ylim(c(0,max(data7$Avg_amount)+5)) +scale_x_discrete(limit = c('Sun','Mon','Tue','Wed','Thu','Fri','Sat'))
+print(plot7)
 
-num_new_donors <- rep(0, 9)
-for (i in 1:10){
-    num_new_donors[i] = length(new_donors(i + 2003))
-}
-length(num_new_donors)
 
-# Plot the results
-plot(num_new_donors, main = "New donors per year", type = 'l', xaxt = "n", xlab = "Year", ylab = "Number of new donors")
-axis(1, 1:10, 2004:2013)
+
+
+
+#- average number of donation for each day of the week
+query8 = "SELECT A.Day, A.ActType, AVG(A.Nb) as Avg_Nb
+          FROM
+          (SELECT YEAR(ActDate) as Year, DAYOFWEEK(ActDate) as Day, ActType, COUNT(Amount) as Nb
+          FROM charity.acts 
+          GROUP BY Year,Day,ActType) A 
+          GROUP BY A.Day, A.ActType "
+
+data8 = sqlQuery(db,query8)
+plot8 = ggplot(data = data8,aes(x=Day,y=Avg_Nb,color=ActType)) + geom_line() + geom_point()+
+  labs(title = 'Average number of donations by day of week',y = 'Average number of donations') +
+  ylim(c(0,max(data8$Avg_Nb)+5)) +scale_x_discrete(limit = c('Sun','Mon','Tue','Wed','Thu','Fri','Sat'))
+print(plot8)
+
+
--- project_code/map_code.R
+++ project_code/map_code.R
@@ -7,25 +7,21 @@ library(RColorBrewer)
 
 db = odbcConnect("MySQL", uid="root")
 
-acts = sqlQuery(db,"SELECT * FROM charity.acts LIMIT 10")
-actions = sqlQuery(db,"SELECT * FROM charity.actions LIMIT 10")
-contacts = sqlQuery(db,"SELECT * FROM charity.contacts LIMIT 10")
-
 querytemp ="SELECT A.ContactId, A.ActType, A.PaymentType, YEAR(A.ActDate) as Year, A.Amount,B.ZipCode, SUBSTRING(B.ZipCode, 1, CHAR_LENGTH(B.ZipCode) - 3) as Dep
           FROM charity.acts A 
           JOIN charity.contacts B
           ON A.ContactId = B.ContactId"
 
-query1 = paste("SELECT A.Dep,A.ActType,AVG(Amount) as Avg_amount,COUNT(Amount) as Nb
+query1 = paste("SELECT A.Dep,A.ActType,AVG(Amount) as Avg_amount
           FROM
 
           (",querytemp,") A
-          WHERE A.Year IN (2011,2012,2013)
-          AND A.Dep IS NOT NULL
+          
+          WHERE A.Dep IS NOT NULL
           GROUP BY A.Dep,A.ActType")
 
 
-# France Map with the average amount per donation for the 3 years 2011,2012,2013 (each department)
+# France Map with the average amount per donation for each department
 data1 = sqlQuery(db,query1)
 
 ## Load map 
@@ -41,7 +37,7 @@ colors_DO <-data1_DO$Avg_amount[match1]/max(data1_DO$Avg_amount)
 # assign 0 when a department is not present 
 colors_DO[is.na(colors_DO)] <- 0
 #plot
-plot(departements,col=gray(1-colors_DO),main='Average amount per donation (DO) during 2011,2012 and 2013')
+plot(departements,col=gray(1-colors_DO),main='Average amount per donation (DO) by "département"')
 
 
 ##### plot for PA #####
@@ -53,7 +49,7 @@ match2 = match(departements$CODE_DEPT,data1_PA$Dep) # match department and data
 colors_PA <-data1_PA$Avg_amount[match2]/max(data1_PA$Avg_amount)
 # assign 0 when a department is not present 
 colors_PA[is.na(colors_PA)] <- 0
-plot(departements,col=gray(1-colors_PA),main='Average amount per donation (PA) during 2011,2012 and 2013')
+plot(departements,col=gray(1-colors_PA),main='Average amount per donation (PA)  by "département"')
 
 
 
